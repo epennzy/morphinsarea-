3e05bc1b2aec6fa6dc23a7849a80df7c7ed20f07
epennzy <epennzy@gmail.com>
2025-07-19 01:09:51 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,10 @@
+import "@/styles/globals.css";
 import { CartProvider } from "@/context/CartContext";
-import { SavedProvider } from "@/context/SavedContext";
 
-export default function MyApp({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
   return (
     <CartProvider>
-      <SavedProvider>
-        <Component {...pageProps} />
-      </SavedProvider>
+      <Component {...pageProps} />
     </CartProvider>
   );
 }
