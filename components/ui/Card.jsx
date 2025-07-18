2a8411efd2840a7e0834f3c3da2841c8b10b7adf
epennzy <epennzy@gmail.com>
2025-07-19 01:18:52 +0700

    Delete components/ui/Card.jsx

--- components/ui/Card.jsx
+++
@@ -1,6 +0,0 @@
-// /components/ui/card.jsx
-
-export function Card({ children }) { return <div className="bg-[#2D2D2D] p-4 rounded-xl shadow text-white">{children}</div>; }
-
-export function CardContent({ children }) { return <div className="mt-2">{children}</div>; }
-
